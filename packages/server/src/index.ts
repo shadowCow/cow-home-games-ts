c331750c251e4d059c57962a9eb36777996f4036
shadowCow <shadowCow84@gmail.com>
2025-12-26 15:32:32 -0500

    scaffold server. use correct org for package names.

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,20 @@
-console.log('Server starting...');
+import Fastify from 'fastify';
+
+const fastify = Fastify({
+  logger: true
+});
+
+fastify.get('/api/hello', async () => {
+  return { message: 'Hello from Cow Home Games server!' };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();

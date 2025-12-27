aa41243353442131ee98484e2fdfc0620fbcd1e6
shadowCow <shadowCow84@gmail.com>
2025-12-27 10:56:48 -0500

    add clean step to drover file.

--- drover.ts
+++ drover.ts
@@ -1,17 +1,20 @@
-import { createBuild, shell, log } from './packages/drover/src/api';
+import { createBuild, shell, log, clean } from './packages/drover/src/api';
 
 export const build = createBuild();
 
 // Define jobs
 const drover = build.job('drover', [
+  clean('packages/drover/dist'),
   shell('npm run build', { cwd: 'packages/drover' })
 ]);
 
 const client = build.job('client', [
+  clean('packages/client/dist'),
   shell('npm run build', { cwd: 'packages/client' })
 ]);
 
 const server = build.job('server', [
+  clean('packages/server/dist'),
   shell('npm run build', { cwd: 'packages/server' })
 ]);
 

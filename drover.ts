974f1ca4a11e370115ad13082e56cfc8e7ae7612
shadowCow <shadowCow84@gmail.com>
2026-01-31 13:52:06 -0500

    fix build order.

--- drover.ts
+++ drover.ts
@@ -13,20 +13,24 @@ const droverBuild = graph.job("drover:build", [
   shell("npm run build", { cwd: "packages/drover" }),
 ]);
 
-const clientBuild = graph.job("client:build", [
-  clean("packages/client/dist"),
-  shell("npm run build", { cwd: "packages/client" }),
-]);
-
-const serverBuild = graph.job("server:build", [
-  clean("packages/server/dist"),
-  shell("npm run build", { cwd: "packages/server" }),
-]);
-
 const protocolBuild = graph.job("protocol:build", [
   clean("packages/protocol/dist"),
   shell("npm run build", { cwd: "packages/protocol" }),
 ]);
+
+const clientBuild = graph
+  .job("client:build", [
+    clean("packages/client/dist"),
+    shell("npm run build", { cwd: "packages/client" }),
+  ])
+  .dependsOn(protocolBuild);
+
+const serverBuild = graph
+  .job("server:build", [
+    clean("packages/server/dist"),
+    shell("npm run build", { cwd: "packages/server" }),
+  ])
+  .dependsOn(protocolBuild);
 
 // Define a job that builds everything
 const build = graph
@@ -39,10 +43,10 @@ graph.job("docker:run", [shell("docker compose up --build")]);
 // Docker registry jobs
 const dockerBuildPi = graph.job("docker:build", [
   log(
-    `Building Docker image for ARM64: ${DOCKER_REGISTRY_HOST}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}`
+    `Building Docker image for ARM64: ${DOCKER_REGISTRY_HOST}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}`,
   ),
   shell(
-    `docker buildx build --platform linux/arm64 -t ${DOCKER_REGISTRY_HOST}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} --push .`
+    `docker buildx build --platform linux/arm64 -t ${DOCKER_REGISTRY_HOST}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} --push .`,
   ),
 ]);
 
